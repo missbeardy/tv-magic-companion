4734eda5e86325594688b826d3d1c660bab33b87
Darren <missbeardy@gmail.com>
2026-06-10 10:47:32 +1000

    PushNotifications Test Web

--- src/lib/oneSignal.ts
+++ src/lib/oneSignal.ts
@@ -3,9 +3,13 @@ import OneSignal from 'react-onesignal';
 export async function initOneSignal() {
   await OneSignal.init({
     appId: '2eeab815-cfc2-4b65-bf79-a3c4415ced61',
+    serviceWorkerParam: { scope: '/' },
     serviceWorkerPath: '/OneSignalSDKWorker.js',
     allowLocalhostAsSecureOrigin: true,
   });
+
+  // Request notification permission immediately after init
+  await OneSignal.Notifications.requestPermission();
 }
 
 export async function setOneSignalUser(userId: string) {
