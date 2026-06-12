f2e8483b958825692d6287b4104a6a6ff2e4a20a
Darren <missbeardy@gmail.com>
2026-06-12 13:41:41 +1000

    brokenpage debug

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,10 @@ function Dashboard() {
 }
 
 function App() {
+  console.log('🔴 App component mounted - checking if this prints')
+
   useEffect(() => {
+    console.log('🔴 useEffect running')
     initOneSignal().catch(err =>
       console.error('OneSignal init failed:', err)
     )
@@ -52,6 +55,7 @@ function App() {
     }
   }, [])
 
+  console.log('🔴 About to return JSX')
   return (
     <AuthProvider>
       <DemoProvider>
